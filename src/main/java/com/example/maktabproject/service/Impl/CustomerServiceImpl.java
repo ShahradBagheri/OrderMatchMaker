74386fb0e2028d3461455f08f7935afbf8515ce0
Shahrad <shahradbagheri6@gmail.com>
2023-10-14 15:03:09 +0330

    created CustomerServiceImpl

---
+++ src/main/java/com/example/maktabproject/service/Impl/CustomerServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.maktabproject.service.Impl;
+
+import com.example.maktabproject.model.Customer;
+import com.example.maktabproject.repository.CustomerRepository;
+import com.example.maktabproject.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public Customer register(Customer customer) {
+        return null;
+    }
+}
