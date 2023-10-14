8cf14ba56c9fd871197e31420439263e6338bc3a
Shahrad <shahradbagheri6@gmail.com>
2023-10-14 14:52:41 +0330

    implemented login

--- src/main/java/com/example/maktabproject/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/maktabproject/service/Impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.maktabproject.service.Impl;
 
+import com.example.maktabproject.exception.IncorrectCrudencialException;
 import com.example.maktabproject.model.User;
 import com.example.maktabproject.repository.UserRepository;
 import com.example.maktabproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +15,10 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
-    public User login(String username, String password) {
+    public User login(String email, String password) throws IncorrectCrudencialException {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new IncorrectCrudencialException("incorrect email or password")
+        );
         return null;
     }
 }
--- src/main/java/com/example/maktabproject/service/UserService.java
+++ src/main/java/com/example/maktabproject/service/UserService.java
@@ -1,9 +1,10 @@
 package com.example.maktabproject.service;
 
+import com.example.maktabproject.exception.IncorrectCrudencialException;
 import com.example.maktabproject.model.User;
 import org.springframework.stereotype.Service;
 
 public interface UserService {
 
-    User login(String email, String password);
+    User login(String email, String password) throws IncorrectCrudencialException;
 }
