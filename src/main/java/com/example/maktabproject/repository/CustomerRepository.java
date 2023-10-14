0376ddae64471821dac16a88683f2c63355561ce
Shahrad <shahradbagheri6@gmail.com>
2023-10-14 15:02:50 +0330

    added extensions

--- src/main/java/com/example/maktabproject/repository/CustomerRepository.java
+++ src/main/java/com/example/maktabproject/repository/CustomerRepository.java
@@ -1,9 +1,10 @@
 package com.example.maktabproject.repository;
 
 import com.example.maktabproject.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 }
