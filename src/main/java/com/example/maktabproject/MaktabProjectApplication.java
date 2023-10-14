b9207dbfd20061868e39f079133f8e8310929bfe
Shahrad <shahradbagheri6@gmail.com>
2023-10-14 10:35:06 +0330

    created project

---
+++ src/main/java/com/example/maktabproject/MaktabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.maktabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaktabProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaktabProjectApplication.class, args);
+    }
+
+}
