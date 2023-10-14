56b7dbd9536dd605bde9eaad953e9a363471798c
Shahrad <shahradbagheri6@gmail.com>
2023-10-14 15:02:12 +0330

    created CustomerRepository

---
+++ src/main/java/com/example/maktabproject/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.maktabproject.repository;
+
+import com.example.maktabproject.model.Customer;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository {
+
+}
