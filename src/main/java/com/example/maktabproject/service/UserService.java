7e05ab6301b14b1a77c6497657ed4d3a7fedfe29
Shahrad <shahradbagheri6@gmail.com>
2023-10-14 14:54:26 +0330

    removed useless import

--- src/main/java/com/example/maktabproject/service/UserService.java
+++ src/main/java/com/example/maktabproject/service/UserService.java
@@ -2,7 +2,6 @@ package com.example.maktabproject.service;
 
 import com.example.maktabproject.exception.IncorrectCrudencialException;
 import com.example.maktabproject.model.User;
-import org.springframework.stereotype.Service;
 
 public interface UserService {
 
