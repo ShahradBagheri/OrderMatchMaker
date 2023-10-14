809de4183736683f325a47d0138e6a973612f378
Shahrad <shahradbagheri6@gmail.com>
2023-10-14 14:52:10 +0330

    created IncorrectCrudencialException

---
+++ src/main/java/com/example/maktabproject/exception/IncorrectCrudencialException.java
@@ -0,0 +1,12 @@
+package com.example.maktabproject.exception;
+
+public class IncorrectCrudencialException extends Exception{
+
+    public IncorrectCrudencialException(){
+        super();
+    }
+
+    public  IncorrectCrudencialException(String message){
+        super(message);
+    }
+}
