75e4b9b97d701889dbb5d103298d242ab7c49471
Shahrad <shahradbagheri6@gmail.com>
2023-10-14 14:27:13 +0330

    added UserRepository as a field

--- src/main/java/com/example/maktabproject/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/maktabproject/service/Impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.maktabproject.service.Impl;
 
 import com.example.maktabproject.model.User;
+import com.example.maktabproject.repository.UserRepository;
 import com.example.maktabproject.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
 
     @Override
     public User register(User user) {
