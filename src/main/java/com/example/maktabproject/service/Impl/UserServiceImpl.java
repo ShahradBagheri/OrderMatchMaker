d020a4ff5bbd60eeb9799e24734bcd1ba524a6b0
Shahrad <shahradbagheri6@gmail.com>
2023-10-14 14:26:06 +0330

    moved @Service

---
+++ src/main/java/com/example/maktabproject/service/Impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.maktabproject.service.Impl;
+
+import com.example.maktabproject.model.User;
+import com.example.maktabproject.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User register(User user) {
+        return null;
+    }
+}
--- src/main/java/com/example/maktabproject/service/UserService.java
+++ src/main/java/com/example/maktabproject/service/UserService.java
@@ -3,7 +3,6 @@ package com.example.maktabproject.service;
 import com.example.maktabproject.model.User;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface UserService {
 
     User register(User user);
