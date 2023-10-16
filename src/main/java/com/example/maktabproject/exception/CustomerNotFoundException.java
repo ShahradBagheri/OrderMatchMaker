1b89d15a8af40f8dd12b194aafe56433ca0a143d
Shahrad <shahradbagheri6@gmail.com>
2023-10-16 17:32:17 +0330

    renamed to customer

---
+++ src/main/java/com/example/maktabproject/exception/CustomerNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.maktabproject.exception;
+
+public class CustomerNotFoundException extends Exception{
+
+    public CustomerNotFoundException() {
+    }
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
