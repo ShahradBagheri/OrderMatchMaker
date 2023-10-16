91aebe0617d5a8634ee68c6eba8082b07ddada5e
Shahrad <shahradbagheri6@gmail.com>
2023-10-16 17:38:02 +0330

    added duplicate email test

--- src/test/java/com/example/maktabproject/service/Impl/CustomerServiceImplSpringTest.java
+++ src/test/java/com/example/maktabproject/service/Impl/CustomerServiceImplSpringTest.java
@@ -2,10 +2,13 @@ package com.example.maktabproject.service.Impl;
 
 import com.example.maktabproject.model.Customer;
 import com.example.maktabproject.model.User;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -27,6 +30,23 @@ class CustomerServiceImplSpringTest {
                 .user(user)
                 .build();
 
+        customer = customerService.register(customer);
+        assertThat(customer.getId()).isNotNull();
+    }
+
+    @Test
+    void duplicateEmailShouldNotSave(){
+        User user = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("shahrad2@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Customer customer = Customer.builder()
+                .user(user)
+                .build();
+
         customerService.register(customer);
+        assertThat(customer.getId()).isNull();
     }
 }
