0bafdd72777853beb83c4400e832cd7e8f560b22
Shahrad <shahradbagheri6@gmail.com>
2023-10-16 16:42:30 +0330

    fixed typo

---
+++ src/main/java/com/example/maktabproject/exception/IncorrectCredentialsException.java
@@ -0,0 +1,12 @@
+package com.example.maktabproject.exception;
+
+public class IncorrectCredentialsException extends Exception{
+
+    public IncorrectCredentialsException(){
+        super();
+    }
+
+    public IncorrectCredentialsException(String message){
+        super(message);
+    }
+}
