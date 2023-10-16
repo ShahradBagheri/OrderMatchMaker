3cc92e13a5380ac284a5c2b5464b4a686d684c2e
Shahrad <shahradbagheri6@gmail.com>
2023-10-16 17:22:30 +0330

    added methods to the interace

--- src/main/java/com/example/maktabproject/service/CustomerService.java
+++ src/main/java/com/example/maktabproject/service/CustomerService.java
@@ -1,8 +1,21 @@
 package com.example.maktabproject.service;
 
 import com.example.maktabproject.model.Customer;
+import com.example.maktabproject.model.User;
+
+import java.util.List;
 
 public interface CustomerService {
 
     Customer register(Customer customer);
+
+    void delete(Customer customer);
+
+    Customer findById(Long id);
+
+    List<Customer> findAll();
+
+    Customer findByUser(User user);
+
+    Customer changePassword(Customer customer,String password);
 }
--- src/main/java/com/example/maktabproject/service/Impl/CustomerServiceImpl.java
+++ src/main/java/com/example/maktabproject/service/Impl/CustomerServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.maktabproject.service.Impl;
 
 import com.example.maktabproject.model.Customer;
+import com.example.maktabproject.model.User;
 import com.example.maktabproject.repository.CustomerRepository;
 import com.example.maktabproject.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +20,33 @@ public class CustomerServiceImpl implements CustomerService {
         try{
             return customerRepository.save(customer);
         } catch (Exception e){
+            System.err.println(e.getMessage());
             return null;
         }
+    }
+
+    @Override
+    public void delete(Customer customer) {
+
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return null;
+    }
+
+    @Override
+    public Customer findByUser(User user) {
+        return null;
+    }
+
+    @Override
+    public Customer changePassword(Customer customer, String password) {
+        return null;
     }
 }
