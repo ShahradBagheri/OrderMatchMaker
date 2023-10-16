4ba88618c774580b1c069c668fc5236b05d0b096
Shahrad <shahradbagheri6@gmail.com>
2023-10-16 17:25:31 +0330

    added find by user

--- src/main/java/com/example/maktabproject/repository/CustomerRepository.java
+++ src/main/java/com/example/maktabproject/repository/CustomerRepository.java
@@ -1,10 +1,14 @@
 package com.example.maktabproject.repository;
 
 import com.example.maktabproject.model.Customer;
+import com.example.maktabproject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+    Optional<Customer> findByUser(User user);
 }
