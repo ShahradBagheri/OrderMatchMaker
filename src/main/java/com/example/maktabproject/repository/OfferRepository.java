7af48380bd0f2de3cbebf19b6666b380a2a31ee0
Shahrad <shahradbagheri6@gmail.com>
2023-10-18 18:18:17 +0330

    created OfferRepository

---
+++ src/main/java/com/example/maktabproject/repository/OfferRepository.java
@@ -0,0 +1,7 @@
+package com.example.maktabproject.repository;
+
+import com.example.maktabproject.model.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+}
