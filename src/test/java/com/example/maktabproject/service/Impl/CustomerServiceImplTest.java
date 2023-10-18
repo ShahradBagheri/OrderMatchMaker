180797367e36c13020ef35c9a58b302b74421841
Shahrad <shahradbagheri6@gmail.com>
2023-10-18 11:55:15 +0330

    added customer tests

--- src/test/java/com/example/maktabproject/service/Impl/CustomerServiceImplTest.java
+++ src/test/java/com/example/maktabproject/service/Impl/CustomerServiceImplTest.java
@@ -11,7 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;````
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
