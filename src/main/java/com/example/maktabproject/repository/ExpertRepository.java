03fa890917090484ab0e8cd8a9a7fb146ab8ccfe
Shahrad <shahradbagheri6@gmail.com>
2023-10-18 14:12:29 +0330

    created expert Repository

---
+++ src/main/java/com/example/maktabproject/repository/ExpertRepository.java
@@ -0,0 +1,7 @@
+package com.example.maktabproject.repository;
+
+import com.example.maktabproject.model.Expert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpertRepository extends JpaRepository<Expert, Long> {
+}
