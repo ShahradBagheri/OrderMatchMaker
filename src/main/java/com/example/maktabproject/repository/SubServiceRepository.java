3f76093d530be01468a02c8f001988220437c051
Shahrad <shahradbagheri6@gmail.com>
2023-10-18 17:58:31 +0330

    created SubServiceRepository interface

---
+++ src/main/java/com/example/maktabproject/repository/SubServiceRepository.java
@@ -0,0 +1,7 @@
+package com.example.maktabproject.repository;
+
+import com.example.maktabproject.model.SubService;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubServiceRepository extends JpaRepository<SubService,Long> {
+}
