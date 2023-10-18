f9bda32302915a5f86689d1803bee73302ef3e11
Shahrad <shahradbagheri6@gmail.com>
2023-10-18 14:18:17 +0330

    added findByUser in ExpertRepository

--- src/main/java/com/example/maktabproject/repository/ExpertRepository.java
+++ src/main/java/com/example/maktabproject/repository/ExpertRepository.java
@@ -1,7 +1,12 @@
 package com.example.maktabproject.repository;
 
 import com.example.maktabproject.model.Expert;
+import com.example.maktabproject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ExpertRepository extends JpaRepository<Expert, Long> {
+
+    Optional<Expert> findByUser(User user);
 }
