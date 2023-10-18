7c42e3f875242c5b69989305aba820298aaad330
Shahrad <shahradbagheri6@gmail.com>
2023-10-18 18:01:44 +0330

    created SubServiceNotFoundException

---
+++ src/main/java/com/example/maktabproject/exception/SubServiceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.maktabproject.exception;
+
+public class SubServiceNotFoundException extends Exception{
+
+    public SubServiceNotFoundException() {
+    }
+
+    public SubServiceNotFoundException(String message) {
+        super(message);
+    }
+}
