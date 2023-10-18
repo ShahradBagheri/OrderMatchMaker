bb70a7f8918c3c6b38d1c3aefbafefc38bbb7a76
Shahrad <shahradbagheri6@gmail.com>
2023-10-18 14:14:22 +0330

    added methods to the interface

--- src/main/java/com/example/maktabproject/service/ExpertService.java
+++ src/main/java/com/example/maktabproject/service/ExpertService.java
@@ -1,4 +1,22 @@
 package com.example.maktabproject.service;
 
+import com.example.maktabproject.exception.CustomerNotFoundException;
+import com.example.maktabproject.model.Expert;
+import com.example.maktabproject.model.User;
+
+import java.util.List;
+
 public interface ExpertService {
+
+    Expert register(Expert expert);
+
+    void delete(Expert expert);
+
+    Expert findById(Long id) throws CustomerNotFoundException;
+
+    List<Expert> findAll();
+
+    Expert findByUser(User user) throws CustomerNotFoundException;
+
+    Expert changePassword(Expert expert,String password);
 }
