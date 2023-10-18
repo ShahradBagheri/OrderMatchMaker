123e316712ba7a5aa526f99c3a6f685696c787ba
Shahrad <shahradbagheri6@gmail.com>
2023-10-18 17:45:46 +0330

    created MainServiceRepository

---
+++ src/main/java/com/example/maktabproject/repository/MainServiceRepository.java
@@ -0,0 +1,7 @@
+package com.example.maktabproject.repository;
+
+import com.example.maktabproject.model.MainService;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MainServiceRepository extends JpaRepository<MainService,Long> {
+}
