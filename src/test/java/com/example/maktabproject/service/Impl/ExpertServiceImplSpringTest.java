30b58033cd3766d686c2d59a4be9a08b4750e8ce
Shahrad <shahradbagheri6@gmail.com>
2023-10-18 14:48:18 +0330

    added tests

--- src/test/java/com/example/maktabproject/service/Impl/ExpertServiceImplSpringTest.java
+++ src/test/java/com/example/maktabproject/service/Impl/ExpertServiceImplSpringTest.java
@@ -8,7 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -103,5 +106,76 @@ class ExpertServiceImplSpringTest {
         Expert foundExpert = expertService.findById(expert.getId());
 
         assertThat(foundExpert).isNotNull();
+    }
+
+    @Test
+    void customersShouldBeFound() {
+        User user = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("shahradTestingAllExpert@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Expert expert = Expert.builder()
+                .user(user)
+                .build();
+
+        expertService.register(expert);
+
+        List<Expert> all = expertService.findAll();
+
+        assertThat(all).isNotNull();
+    }
+
+    @Test
+    void CustomerShouldGetDeleted() throws CustomerNotFoundException {
+        User user = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("shahradexpertdelete@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Expert expert = Expert.builder()
+                .user(user)
+                .build();
+
+        Expert registerdExpert = expertService.register(expert);
+        expertService.delete(expertService.findById(registerdExpert.getId()));
+        assertThatThrownBy(() -> expertService.findById(registerdExpert.getId())).isInstanceOf(CustomerNotFoundException.class);
+    }
+
+    @Test
+    void CustomerPasswordShouldChange() throws CustomerNotFoundException {
+        User user = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("shahradchangedexpertpassword@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Expert expert = Expert.builder()
+                .user(user)
+                .build();
+
+        expert = expertService.register(expert);
+        expertService.changePassword(expert, "changed123");
+        Expert changedExpert = expertService.findById(expert.getId());
+        assertThat(changedExpert.getUser().getPassword()).isEqualTo("changed123");
+    }
+
+    @Test
+    void CustomerFoundByUser() throws CustomerNotFoundException {
+        User user = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("findUserTestExpert@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Expert expert = Expert.builder()
+                .user(user)
+                .build();
+
+        expert = expertService.register(expert);
+        Expert foundExpert = expertService.findByUser(expert.getUser());
+        assertThat(expert.getUser()).isEqualTo(foundExpert.getUser());
     }
 }
