4449fa0bf41ca266d4e33b5e4eae9d847a024c60
Shahrad <shahradbagheri6@gmail.com>
2023-10-20 03:03:28 +0330

    created exception

---
+++ src/main/java/com/example/maktabproject/exception/ExpertHasNoOfferForOfferException.java
@@ -0,0 +1,11 @@
+package com.example.maktabproject.exception;
+
+public class ExpertHasNoOfferForOfferException extends Exception{
+
+    public ExpertHasNoOfferForOfferException() {
+    }
+
+    public ExpertHasNoOfferForOfferException(String message) {
+        super(message);
+    }
+}
