cb56f71f04894c2d68a90f9a5be4ffb7576c2345
Shahrad <shahradbagheri6@gmail.com>
2023-10-19 18:00:48 +0330

    created OrderTest

---
+++ src/test/java/com/example/maktabproject/service/Impl/OrderServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.example.maktabproject.service.Impl;
+
+import org.junit.jupiter.api.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OrderServiceImplTest {
+
+    @Autowired
+    private OrderServiceImpl orderService;
+
+
+}
