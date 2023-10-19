8c9699671538eb061185e407434dbc07d1c4ccb3
Shahrad <shahradbagheri6@gmail.com>
2023-10-20 01:31:12 +0330

    added offer register tests

--- src/test/java/com/example/maktabproject/service/Impl/OfferServiceImplTest.java
+++ src/test/java/com/example/maktabproject/service/Impl/OfferServiceImplTest.java
@@ -1,8 +1,16 @@
 package com.example.maktabproject.service.Impl;
 
+import com.example.maktabproject.exception.InvalidPriceException;
+import com.example.maktabproject.exception.InvalidTimeException;
+import com.example.maktabproject.model.*;
+import com.example.maktabproject.model.enumeration.OrderState;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -10,4 +18,116 @@ class OfferServiceImplTest {
 
     @Autowired
     private OfferServiceImpl offerService;
+
+    @Autowired
+    private ExpertServiceImpl expertService;
+
+    @Autowired
+    private SubServiceServiceImpl subServiceService;
+
+    @Autowired
+    private CustomerServiceImpl customerService;
+
+    @Autowired
+    private OrderServiceImpl orderService;
+
+    @Test
+    void validOfferShouldSave() throws InvalidPriceException, InvalidTimeException {
+        User user = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("offersavetest@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Expert expert = Expert.builder()
+                .user(user)
+                .build();
+
+        expert = expertService.register(expert);
+
+        SubService subService = SubService.builder()
+                .name("testingofferregister")
+                .basePrice(100.0)
+                .build();
+
+        subServiceService.register(subService);
+
+        User user2 = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("testingofferaddsavecustomer@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Customer customer = Customer.builder()
+                .user(user2)
+                .build();
+
+        customer = customerService.register(customer);
+
+        Order order = Order.builder()
+                .orderState(OrderState.WAITING_FOR_SUGGESTIONS)
+                .address("Some address")
+                .subService(subService)
+                .customer(customer)
+                .suggestedPrice(200.0)
+                .startingDate(LocalDateTime.now().plusDays(1))
+                .build();
+
+        orderService.register(order);
+
+        Offer offer = Offer.builder()
+                .expert(expert)
+                .order(order)
+                .suggestedPrice(200.0)
+                .startingDate(LocalDateTime.now().plusDays(1))
+                .completionDate(LocalDateTime.now().plusDays(3))
+                .build();
+
+        offer = offerService.register(offer);
+        assertThat(offer.getId()).isNotNull();
+    }
+
+    @Test
+    void offerWithNoExpertShouldNotSave() throws InvalidPriceException, InvalidTimeException {
+
+        SubService subService = SubService.builder()
+                .name("noExpertOfferTest")
+                .basePrice(100.0)
+                .build();
+
+        subServiceService.register(subService);
+
+        User user2 = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("noExpertOfferTest@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Customer customer = Customer.builder()
+                .user(user2)
+                .build();
+
+        customer = customerService.register(customer);
+
+        Order order = Order.builder()
+                .orderState(OrderState.WAITING_FOR_SUGGESTIONS)
+                .address("Some address")
+                .subService(subService)
+                .customer(customer)
+                .suggestedPrice(200.0)
+                .startingDate(LocalDateTime.now().plusDays(1))
+                .build();
+
+        orderService.register(order);
+
+        Offer offer = Offer.builder()
+                .order(order)
+                .suggestedPrice(200.0)
+                .startingDate(LocalDateTime.now().plusDays(1))
+                .completionDate(LocalDateTime.now().plusDays(3))
+                .build();
+
+        offer = offerService.register(offer);
+        assertThat(offer).isNull();
+    }
 }
