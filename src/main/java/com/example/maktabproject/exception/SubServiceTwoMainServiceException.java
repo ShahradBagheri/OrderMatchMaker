5ae4423cebdcd7fb2ad4615f36ef5b19d18c4431
Shahrad <shahradbagheri6@gmail.com>
2023-10-19 08:40:01 +0330

    created exception

---
+++ src/main/java/com/example/maktabproject/exception/SubServiceTwoMainServiceException.java
@@ -0,0 +1,11 @@
+package com.example.maktabproject.exception;
+
+public class SubServiceTwoMainServiceException extends Exception{
+
+    public SubServiceTwoMainServiceException() {
+    }
+
+    public SubServiceTwoMainServiceException(String message) {
+        super(message);
+    }
+}
