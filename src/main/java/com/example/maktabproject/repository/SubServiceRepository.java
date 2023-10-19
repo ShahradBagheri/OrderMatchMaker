2aaf87e2eb6900675088543407d13a182f0614f6
Shahrad <shahradbagheri6@gmail.com>
2023-10-19 12:46:52 +0330

    added findAllByMainService

--- src/main/java/com/example/maktabproject/repository/SubServiceRepository.java
+++ src/main/java/com/example/maktabproject/repository/SubServiceRepository.java
@@ -1,7 +1,12 @@
 package com.example.maktabproject.repository;
 
+import com.example.maktabproject.model.MainService;
 import com.example.maktabproject.model.SubService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubServiceRepository extends JpaRepository<SubService,Long> {
+
+    List<SubService> findAllByMainService(MainService mainService);
 }
