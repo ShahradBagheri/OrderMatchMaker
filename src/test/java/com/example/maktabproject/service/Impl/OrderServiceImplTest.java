3550c2abd4c243de304a0bf5610b3c1097874979
Shahrad <shahradbagheri6@gmail.com>
2023-10-19 18:08:31 +0330

    added shouldSave test

--- src/test/java/com/example/maktabproject/service/Impl/OrderServiceImplTest.java
+++ src/test/java/com/example/maktabproject/service/Impl/OrderServiceImplTest.java
@@ -1,9 +1,18 @@
 package com.example.maktabproject.service.Impl;
 
-import org.junit.jupiter.api.Order;
+import com.example.maktabproject.exception.InvalidPriceException;
+import com.example.maktabproject.model.Customer;
+import com.example.maktabproject.model.Order;
+import com.example.maktabproject.model.SubService;
+import com.example.maktabproject.model.User;
+import com.example.maktabproject.model.enumeration.OrderState;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -12,5 +21,44 @@ class OrderServiceImplTest {
     @Autowired
     private OrderServiceImpl orderService;
 
+    @Autowired
+    private SubServiceServiceImpl subServiceService;
 
+    @Autowired
+    private CustomerServiceImpl customerService;
+
+    @Test
+    void orderShouldSave() throws InvalidPriceException {
+
+        SubService subService = SubService.builder()
+                .name("orderShouldSave")
+                .basePrice(100.0)
+                .build();
+
+        subServiceService.register(subService);
+
+        User user = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("registerOrderTest@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Customer customer = Customer.builder()
+                .user(user)
+                .build();
+
+        customer = customerService.register(customer);
+
+        Order order = Order.builder()
+                .orderState(OrderState.STARTED)
+                .address("Some address")
+                .subService(subService)
+                .customer(customer)
+                .suggestedPrice(200.0)
+                .startingDate(LocalDateTime.now().plusDays(1))
+                .build();
+
+        order = orderService.register(order);
+        assertThat(order).isNotNull();
+    }
 }
