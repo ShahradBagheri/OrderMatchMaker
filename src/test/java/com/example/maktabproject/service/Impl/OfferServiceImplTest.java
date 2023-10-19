1083e053c3ae2b1d65809c620eaca93c08fb3be6
Shahrad <shahradbagheri6@gmail.com>
2023-10-19 21:30:44 +0330

    created test

---
+++ src/test/java/com/example/maktabproject/service/Impl/OfferServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.example.maktabproject.service.Impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OfferServiceImplTest {
+
+    @Autowired
+    private OfferServiceImpl offerService;
+}
