d84951dc18d99346b28e73eeec16a27e401cb540
Shahrad <shahradbagheri6@gmail.com>
2023-10-20 01:43:12 +0330

    added invalid price and time tests

--- src/test/java/com/example/maktabproject/service/Impl/OfferServiceImplTest.java
+++ src/test/java/com/example/maktabproject/service/Impl/OfferServiceImplTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -129,5 +130,115 @@ class OfferServiceImplTest {
 
         offer = offerService.register(offer);
         assertThat(offer).isNull();
+    }
+
+    @Test
+    void invalidTimeShouldNotSave() throws InvalidPriceException, InvalidTimeException {
+        User user = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("invalidTimeShouldNotSave2@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Expert expert = Expert.builder()
+                .user(user)
+                .build();
+
+        expert = expertService.register(expert);
+
+        SubService subService = SubService.builder()
+                .name("invalidTimeShouldNotSave")
+                .basePrice(100.0)
+                .build();
+
+        subServiceService.register(subService);
+
+        User user2 = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("invalidTimeShouldNotSave@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Customer customer = Customer.builder()
+                .user(user2)
+                .build();
+
+        customer = customerService.register(customer);
+
+        Order order = Order.builder()
+                .orderState(OrderState.WAITING_FOR_SUGGESTIONS)
+                .address("Some address")
+                .subService(subService)
+                .customer(customer)
+                .suggestedPrice(200.0)
+                .startingDate(LocalDateTime.now().plusDays(1))
+                .build();
+
+        orderService.register(order);
+
+        Offer offer = Offer.builder()
+                .expert(expert)
+                .order(order)
+                .suggestedPrice(200.0)
+                .startingDate(LocalDateTime.now().minusDays(1))
+                .completionDate(LocalDateTime.now().plusDays(3))
+                .build();
+
+        assertThatThrownBy(() -> offerService.register(offer)).isInstanceOf(InvalidTimeException.class);
+    }
+
+    @Test
+    void invalidPriceShouldNotBeSaved() throws InvalidPriceException, InvalidTimeException {
+        User user = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("invalidPriceSHouldNotBeSavedOffer@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Expert expert = Expert.builder()
+                .user(user)
+                .build();
+
+        expert = expertService.register(expert);
+
+        SubService subService = SubService.builder()
+                .name("invalidPriceSHouldNotBeSavedOffer")
+                .basePrice(100.0)
+                .build();
+
+        subServiceService.register(subService);
+
+        User user2 = User.builder()
+                .firstname("shahrad")
+                .lastname("bagheri")
+                .email("invalidPriceSHouldNotBeSavedOffer2@gmaill.com")
+                .password("qweasd123")
+                .build();
+        Customer customer = Customer.builder()
+                .user(user2)
+                .build();
+
+        customer = customerService.register(customer);
+
+        Order order = Order.builder()
+                .orderState(OrderState.WAITING_FOR_SUGGESTIONS)
+                .address("Some address")
+                .subService(subService)
+                .customer(customer)
+                .suggestedPrice(200.0)
+                .startingDate(LocalDateTime.now().plusDays(1))
+                .build();
+
+        orderService.register(order);
+
+        Offer offer = Offer.builder()
+                .expert(expert)
+                .order(order)
+                .suggestedPrice(50.0)
+                .startingDate(LocalDateTime.now().minusDays(1))
+                .completionDate(LocalDateTime.now().plusDays(3))
+                .build();
+
+        assertThatThrownBy(() -> offerService.register(offer)).isInstanceOf(InvalidPriceException.class);
     }
 }
