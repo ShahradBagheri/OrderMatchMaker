8e28d8fd7842940c74281136e1933059ecdcbf5a
Shahrad <shahradbagheri6@gmail.com>
2023-10-19 20:57:01 +0330

    added new method to OrderRepository

--- src/main/java/com/example/maktabproject/repository/OrderRepository.java
+++ src/main/java/com/example/maktabproject/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.example.maktabproject.repository;
 
 import com.example.maktabproject.model.Order;
+import com.example.maktabproject.model.SubService;
+import com.example.maktabproject.model.enumeration.OrderState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findBySubServiceInAndOrderStateOrOrderState(List<SubService> subServices, OrderState orderState1, OrderState orderState2);
 }
