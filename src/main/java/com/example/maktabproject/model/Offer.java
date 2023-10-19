9cb971c4f8af8f690a6c97a3a9a35ac117e2d4ec
Shahrad <shahradbagheri6@gmail.com>
2023-10-20 00:45:11 +0330

    added @Builder

--- src/main/java/com/example/maktabproject/model/Offer.java
+++ src/main/java/com/example/maktabproject/model/Offer.java
@@ -1,6 +1,7 @@
 package com.example.maktabproject.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 public class Offer {
 
