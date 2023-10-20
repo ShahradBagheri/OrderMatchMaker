66c78e4d18ac8bc2afaf23818315e1860d5b86c4
Shahrad <shahradbagheri6@gmail.com>
2023-10-20 04:57:29 +0330

    created exception

---
+++ src/main/java/com/example/maktabproject/exception/ImageToBigException.java
@@ -0,0 +1,11 @@
+package com.example.maktabproject.exception;
+
+public class ImageToBigException extends Exception{
+
+    public ImageToBigException() {
+    }
+
+    public ImageToBigException(String message) {
+        super(message);
+    }
+}
