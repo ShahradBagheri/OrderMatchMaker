f796892c06356d9e82cac65fd25c2443eb94aa4d
Shahrad <shahradbagheri6@gmail.com>
2023-10-26 09:43:38 +0330

    changed repository method to find by Id

--- src/main/java/com/example/maktabproject/repository/CustomerRepository.java
+++ src/main/java/com/example/maktabproject/repository/CustomerRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    Optional<Customer> findByUser(User user);
+    Optional<Customer> findByUser_Id(Long userId);
 }
