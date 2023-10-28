3d3ef723e95a436a7494404b8a9d6dd3077ee162
Shahrad <shahradbagheri6@gmail.com>
2023-10-28 15:12:56 +0330

    added saving image capability to expert registery

--- src/main/java/com/example/maktabproject/controller/UserController.java
+++ src/main/java/com/example/maktabproject/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.maktabproject.controller;
 
 import com.example.maktabproject.dto.*;
+import com.example.maktabproject.exception.ImageToBigException;
 import com.example.maktabproject.exception.IncorrectCredentialsException;
 import com.example.maktabproject.model.Customer;
 import com.example.maktabproject.model.Expert;
@@ -8,11 +9,15 @@ import com.example.maktabproject.model.User;
 import com.example.maktabproject.service.Impl.CustomerServiceImpl;
 import com.example.maktabproject.service.Impl.ExpertServiceImpl;
 import com.example.maktabproject.service.Impl.UserServiceImpl;
+import com.example.maktabproject.util.ImageProcessing;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/user")
@@ -23,6 +28,7 @@ public class UserController {
     private final UserMapper userMapper;
     private final CustomerServiceImpl customerService;
     private final ExpertServiceImpl expertService;
+    private final ImageProcessing imageProcessing;
 
     @PostMapping("/login")
     public ResponseEntity<UserResponseDto> login(@RequestBody UserLoginDto userLoginDto) throws IncorrectCredentialsException {
@@ -41,9 +47,11 @@ public class UserController {
     }
 
     @PostMapping("/register/expert")
-    public UserResponseDto expertRegister(@RequestBody @Valid UserRequestDto userRequestDto){
+    public UserResponseDto expertRegister(@RequestBody @Valid UserRequestDto userRequestDto, @RequestPart MultipartFile image) throws IOException, ImageToBigException {
 
+        byte[] imageData = imageProcessing.imageToBytes(image);
         Expert expert = userMapper.userDtoToExpert(userRequestDto);
+        expert.setImageData(imageData);
         expert = expertService.register(expert);
         return userMapper.userToUserDto(expert.getUser());
     }
