0f367e76045953e073301323e566782d1258af44
Shahrad <shahradbagheri6@gmail.com>
2023-10-28 15:19:14 +0330

    added NotBlank to firstname and lastname

--- src/main/java/com/example/maktabproject/dto/UserRequestDto.java
+++ src/main/java/com/example/maktabproject/dto/UserRequestDto.java
@@ -2,14 +2,14 @@ package com.example.maktabproject.dto;
 
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
-public record UserRequestDto(String firstname,
-                             String lastname,
-                             @Email
-                             String email,
+public record UserRequestDto(@NotBlank String firstname,
+                             @NotBlank String lastname,
+                             @Email String email,
                              @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).{8,}$")
                              String password) {
 }
