6e911a2482ea57cf0deb765cf9a6415c5d648c6d
Shahrad <shahradbagheri6@gmail.com>
2023-10-28 15:33:34 +0330

    created register mainService to AdminController

--- src/main/java/com/example/maktabproject/controller/AdminController.java
+++ src/main/java/com/example/maktabproject/controller/AdminController.java
@@ -1,14 +1,13 @@
 package com.example.maktabproject.controller;
 
 import com.example.maktabproject.dto.MainServiceMapper;
+import com.example.maktabproject.dto.MainServiceRequestDto;
 import com.example.maktabproject.dto.MainServiceResponseDto;
 import com.example.maktabproject.model.MainService;
 import com.example.maktabproject.service.AdminService;
 import com.example.maktabproject.service.Impl.MainServiceServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +26,12 @@ public class AdminController {
         List<MainService> mainServiceList = mainServiceService.findAll();
         List<MainServiceResponseDto> list = mainServiceList.stream().map(mainServiceMapper::mainServiceToDto).toList();
         return list;
+    }
+
+    @PostMapping("/register/MainService")
+    public MainServiceResponseDto registerMainService(@RequestBody MainServiceRequestDto mainServiceRequestDto){
+
+        MainService mainService = mainServiceService.register(mainServiceMapper.mainServiceDtoToMainService(mainServiceRequestDto));
+        return mainServiceMapper.mainServiceToDto(mainService);
     }
 }
