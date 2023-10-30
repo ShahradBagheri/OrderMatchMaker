e968bb9f98f2edfbf2d85421b7fd37adeeccb9ef
Shahrad <shahradbagheri6@gmail.com>
2023-10-30 18:27:58 +0330

    added @Validated

--- src/main/java/com/example/maktabproject/dto/SubServiceRequestDto.java
+++ src/main/java/com/example/maktabproject/dto/SubServiceRequestDto.java
@@ -3,8 +3,10 @@ package com.example.maktabproject.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import org.springframework.validation.annotation.Validated;
 
 @Builder
+@Validated
 public record SubServiceRequestDto(@NotBlank String name,
                                    @NotNull Double basePrice,
                                    @NotBlank String Comment) {
