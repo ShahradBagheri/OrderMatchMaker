fec908278a693ec9a2a84bd1b31aa4ce4334e9de
Shahrad <shahradbagheri6@gmail.com>
2023-11-02 01:47:52 +0330

    implemented the expert part of the filter

--- src/main/java/com/example/maktabproject/service/AdminService.java
+++ src/main/java/com/example/maktabproject/service/AdminService.java
@@ -1,10 +1,14 @@
 package com.example.maktabproject.service;
 
+import com.example.maktabproject.dto.UserFilterCriteriaDto;
 import com.example.maktabproject.exception.ExpertNotFoundException;
 import com.example.maktabproject.exception.SubServiceNotFoundException;
 import com.example.maktabproject.model.Expert;
 import com.example.maktabproject.model.SubService;
+import com.example.maktabproject.model.User;
 import com.example.maktabproject.model.enumeration.ExpertStatus;
+
+import java.util.List;
 
 public interface AdminService {
 
@@ -17,4 +21,6 @@ public interface AdminService {
     SubService editSubServicePrice(Long subServiceId, Double newBasePrice) throws SubServiceNotFoundException;
 
     SubService editSubServiceComment(Long subServiceId, String newComment) throws SubServiceNotFoundException;
+
+    List<User> filterUsers(UserFilterCriteriaDto userFilterCriteriaDto);
 }
--- src/main/java/com/example/maktabproject/service/Impl/AdminServiceImpl.java
+++ src/main/java/com/example/maktabproject/service/Impl/AdminServiceImpl.java
@@ -1,14 +1,20 @@
 package com.example.maktabproject.service.Impl;
 
+import com.example.maktabproject.dto.UserFilterCriteriaDto;
 import com.example.maktabproject.exception.ExpertNotFoundException;
 import com.example.maktabproject.exception.SubServiceNotFoundException;
 import com.example.maktabproject.model.Expert;
 import com.example.maktabproject.model.SubService;
+import com.example.maktabproject.model.User;
 import com.example.maktabproject.model.enumeration.ExpertStatus;
+import com.example.maktabproject.repository.ExpertRepository;
 import com.example.maktabproject.service.AdminService;
 import com.example.maktabproject.service.SubServiceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +22,7 @@ public class AdminServiceImpl implements AdminService {
 
     private final SubServiceServiceImpl subServiceService;
     private final ExpertServiceImpl expertService;
+    private final ExpertRepository expertRepository;
 
     @Override
     public Expert addExpertSubService(Long expertId, Long subServiceId) throws ExpertNotFoundException, SubServiceNotFoundException {
@@ -64,5 +71,20 @@ public class AdminServiceImpl implements AdminService {
         subService.setComment(newComment);
 
         return subServiceService.register(subService);
+    }
+
+    @Override
+    public List<User> filterUsers(UserFilterCriteriaDto userFilterCriteriaDto) {
+
+        Specification<Expert> specification = Specification.where(null);
+
+        if(userFilterCriteriaDto.score() != null)
+            specification = specification.and((root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get("score"), userFilterCriteriaDto.score()));
+
+        if(userFilterCriteriaDto.subService() != null)
+            specification = specification.and((root, query, criteriaBuilder) -> criteriaBuilder.isMember(userFilterCriteriaDto.subService(),root.get("subServices")));
+
+
+        return expertRepository.findAll(specification).stream().map(Expert::getUser).toList();
     }
 }
