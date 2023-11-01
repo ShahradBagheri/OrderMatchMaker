f3697df3d11870f4c0f462ab1aa756df4c707b94
Shahrad <shahradbagheri6@gmail.com>
2023-11-02 01:47:36 +0330

    changed subServiceId to subService

--- src/main/java/com/example/maktabproject/dto/UserFilterCriteriaDto.java
+++ src/main/java/com/example/maktabproject/dto/UserFilterCriteriaDto.java
@@ -1,11 +1,14 @@
 package com.example.maktabproject.dto;
 
+import com.example.maktabproject.model.SubService;
 import com.example.maktabproject.model.enumeration.Role;
+import lombok.Builder;
 
+@Builder
 public record UserFilterCriteriaDto(Role role,
                                     String firstname,
                                     String lastname,
                                     String email,
-                                    Long subServiceId,
+                                    SubService subService,
                                     Float score) {
 }
