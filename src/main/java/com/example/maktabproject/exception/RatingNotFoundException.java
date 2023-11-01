25d72391cdb616a5b0db66543f132310b7ec59ee
Shahrad <shahradbagheri6@gmail.com>
2023-11-01 22:37:35 +0330

    created exception

---
+++ src/main/java/com/example/maktabproject/exception/RatingNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.maktabproject.exception;
+
+public class RatingNotFoundException extends Exception{
+
+    public RatingNotFoundException() {
+    }
+
+    public RatingNotFoundException(String message) {
+        super(message);
+    }
+}
