a3881248c8b1ae1d53ac8746adfbf3c8543550c7
Shahrad <shahradbagheri6@gmail.com>
2023-11-02 19:13:42 +0330

    implemented payment method

--- src/main/java/com/example/maktabproject/controller/PaymentController.java
+++ src/main/java/com/example/maktabproject/controller/PaymentController.java
@@ -1,11 +1,14 @@
 package com.example.maktabproject.controller;
 
+import com.example.maktabproject.dto.PaymentRequestDto;
+import com.example.maktabproject.exception.InvalidCaptchaException;
+import com.example.maktabproject.exception.UserNotFoundException;
+import com.example.maktabproject.model.User;
+import com.example.maktabproject.service.Impl.UserServiceImpl;
 import com.wf.captcha.SpecCaptcha;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.relational.core.sql.In;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -18,6 +21,20 @@ public class PaymentController {
 
     private Map<Integer,String> captchaMap = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger();
+    private final UserServiceImpl userService;
+
+    @PostMapping("/submit")
+    public String payment(@RequestBody PaymentRequestDto paymentRequestDto) throws InvalidCaptchaException, UserNotFoundException {
+
+        String captchaText = captchaMap.remove(paymentRequestDto.captchaId());
+        if(! paymentRequestDto.captcha().equals(captchaText))
+            throw new  InvalidCaptchaException();
+
+        User user = userService.findById(paymentRequestDto.userId());
+        user.getWallet().setCredit(user.getWallet().getCredit() + paymentRequestDto.amount());
+        userService.register(user);
+        return "MONEY ADDED!";
+    }
 
     @GetMapping("/captcha")
     public Captcha generateCaptcha() {
