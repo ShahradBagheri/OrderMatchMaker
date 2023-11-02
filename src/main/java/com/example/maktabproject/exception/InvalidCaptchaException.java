0349a27ac301af2dde65cf098a69da6c8849d403
Shahrad <shahradbagheri6@gmail.com>
2023-11-02 19:12:30 +0330

    created exception

---
+++ src/main/java/com/example/maktabproject/exception/InvalidCaptchaException.java
@@ -0,0 +1,11 @@
+package com.example.maktabproject.exception;
+
+public class InvalidCaptchaException extends Exception{
+
+    public InvalidCaptchaException() {
+    }
+
+    public InvalidCaptchaException(String message) {
+        super(message);
+    }
+}
