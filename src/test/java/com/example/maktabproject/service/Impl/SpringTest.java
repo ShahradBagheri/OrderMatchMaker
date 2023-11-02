0de2b22d5168adb060c3a52242e4349faa621b10
Shahrad <shahradbagheri6@gmail.com>
2023-11-02 15:23:53 +0330

    added filter test

---
+++ src/test/java/com/example/maktabproject/service/Impl/SpringTest.java
@@ -0,0 +1,34 @@
+package com.example.maktabproject.service.Impl;
+
+import com.example.maktabproject.dto.UserFilterCriteriaDto;
+import com.example.maktabproject.exception.SubServiceNotFoundException;
+import com.example.maktabproject.model.SubService;
+import com.example.maktabproject.model.User;
+import com.example.maktabproject.model.enumeration.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class SpringTest {
+
+    @Autowired
+    private AdminServiceImpl adminService;
+
+    @Autowired
+    private SubServiceServiceImpl subServiceService;
+
+    @Test
+    void run() throws SubServiceNotFoundException {
+
+        SubService byId = subServiceService.findById(1L);
+
+        UserFilterCriteriaDto build = UserFilterCriteriaDto.builder()
+                .email("z")
+                .build();
+        List<User> users = adminService.filterUsers(build);
+        System.out.println(users.stream().map(User::getId).toList());
+    }
+}
