a2aa0bd34c18ac34e74d01d10b9656e88910f903
Shahrad <shahradbagheri6@gmail.com>
2023-11-02 11:35:38 +0330

    added user filter options to filterUsers

--- src/main/java/com/example/maktabproject/service/Impl/AdminServiceImpl.java
+++ src/main/java/com/example/maktabproject/service/Impl/AdminServiceImpl.java
@@ -8,12 +8,14 @@ import com.example.maktabproject.model.SubService;
 import com.example.maktabproject.model.User;
 import com.example.maktabproject.model.enumeration.ExpertStatus;
 import com.example.maktabproject.repository.ExpertRepository;
+import com.example.maktabproject.repository.UserRepository;
 import com.example.maktabproject.service.AdminService;
 import com.example.maktabproject.service.SubServiceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,6 +25,7 @@ public class AdminServiceImpl implements AdminService {
     private final SubServiceServiceImpl subServiceService;
     private final ExpertServiceImpl expertService;
     private final ExpertRepository expertRepository;
+    private final UserRepository userRepository;
 
     @Override
     public Expert addExpertSubService(Long expertId, Long subServiceId) throws ExpertNotFoundException, SubServiceNotFoundException {
@@ -76,15 +79,39 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public List<User> filterUsers(UserFilterCriteriaDto userFilterCriteriaDto) {
 
-        Specification<Expert> specification = Specification.where(null);
+        Specification<Expert> expertSpecification = Specification.where(null);
+        Specification<User> userSpecification = Specification.where(null);
 
         if(userFilterCriteriaDto.score() != null)
-            specification = specification.and((root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get("score"), userFilterCriteriaDto.score()));
+            expertSpecification = expertSpecification.and((root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get("score"), userFilterCriteriaDto.score()));
 
         if(userFilterCriteriaDto.subService() != null)
-            specification = specification.and((root, query, criteriaBuilder) -> criteriaBuilder.isMember(userFilterCriteriaDto.subService(),root.get("subServices")));
+            expertSpecification = expertSpecification.and((root, query, criteriaBuilder) -> criteriaBuilder.isMember(userFilterCriteriaDto.subService(),root.get("subServices")));
 
+        if(userFilterCriteriaDto.firstname() != null)
+            userSpecification = userSpecification.and((root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("firstname"),"%" + userFilterCriteriaDto.firstname() + "%"));
 
-        return expertRepository.findAll(specification).stream().map(Expert::getUser).toList();
+        if(userFilterCriteriaDto.lastname() != null)
+            userSpecification = userSpecification.and((root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("lastname"),"%" + userFilterCriteriaDto.lastname() + "%"));
+
+        if(userFilterCriteriaDto.email() != null)
+            userSpecification = userSpecification.and((root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("email"),"%" + userFilterCriteriaDto.email() + "%"));
+
+        if(userFilterCriteriaDto.role() != null)
+            userSpecification = userSpecification.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("role"),userFilterCriteriaDto.role()));
+
+        List<User> expertUsers = expertRepository.findAll(expertSpecification).stream().map(Expert::getUser).toList();
+        List<User> allUsers = userRepository.findAll(userSpecification);
+        List<User> commonElements = new ArrayList<>();
+
+        if(userFilterCriteriaDto.score() != null || userFilterCriteriaDto.subService() != null){
+            for (User expert : expertUsers) {
+                if(allUsers.contains(expert) && !commonElements.contains(expert))
+                    commonElements.add(expert);
+            }
+            return commonElements;
+        }
+
+        return allUsers;
     }
 }
