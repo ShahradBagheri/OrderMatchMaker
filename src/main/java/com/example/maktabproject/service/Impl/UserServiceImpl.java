8688bac91d2f35795c5507034ba1a38eb6fa3032
Shahrad <shahradbagheri6@gmail.com>
2023-11-02 19:12:19 +0330

    added findById and register methods

--- src/main/java/com/example/maktabproject/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/maktabproject/service/Impl/UserServiceImpl.java
@@ -1,17 +1,33 @@
 package com.example.maktabproject.service.Impl;
 
 import com.example.maktabproject.exception.IncorrectCredentialsException;
+import com.example.maktabproject.exception.UserNotFoundException;
 import com.example.maktabproject.model.User;
 import com.example.maktabproject.repository.UserRepository;
 import com.example.maktabproject.service.UserService;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    @Override
+    public User register(User user) {
+
+        try{
+            return userRepository.save(user);
+        } catch (ConstraintViolationException | DataAccessException e){
+            log.error(e.getMessage());
+            return null;
+        }
+    }
 
     @Override
     public User login(String email, String password) throws IncorrectCredentialsException {
@@ -19,4 +35,13 @@ public class UserServiceImpl implements UserService {
                 () -> new IncorrectCredentialsException("incorrect email or password")
         );
     }
+
+    @Override
+    public User findById(Long id) throws UserNotFoundException {
+        return userRepository.findById(id).orElseThrow(
+                UserNotFoundException::new
+        );
+    }
+
+
 }
--- src/main/java/com/example/maktabproject/service/UserService.java
+++ src/main/java/com/example/maktabproject/service/UserService.java
@@ -1,9 +1,14 @@
 package com.example.maktabproject.service;
 
 import com.example.maktabproject.exception.IncorrectCredentialsException;
+import com.example.maktabproject.exception.UserNotFoundException;
 import com.example.maktabproject.model.User;
 
 public interface UserService {
 
+    User register(User user);
+
     User login(String email, String password) throws IncorrectCredentialsException;
+
+    User findById(Long id) throws UserNotFoundException;
 }
