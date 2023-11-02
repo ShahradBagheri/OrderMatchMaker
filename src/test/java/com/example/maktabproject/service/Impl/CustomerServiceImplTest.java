624f1906dd2a1f2d5e60551f828af6e7496e2dbd
Shahrad <shahradbagheri6@gmail.com>
2023-11-03 00:36:40 +0330

    wrote a test to check orderState when changed to started

--- src/test/java/com/example/maktabproject/service/Impl/CustomerServiceImplTest.java
+++ src/test/java/com/example/maktabproject/service/Impl/CustomerServiceImplTest.java
@@ -5,6 +5,7 @@ package com.example.maktabproject.service.Impl;
 //import com.example.maktabproject.repository.CustomerRepository;
 //import org.junit.jupiter.api.BeforeEach;
 //import org.junit.jupiter.api.Test;
+import com.example.maktabproject.model.Customer;
 import org.junit.jupiter.api.extension.ExtendWith;
 //import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
---
+++ src/test/java/com/example/maktabproject/service/Impl/OrderServiceTest.java
@@ -0,0 +1,67 @@
+package com.example.maktabproject.service.Impl;
+
+import com.example.maktabproject.exception.InvalidPriceException;
+import com.example.maktabproject.exception.InvalidTimeException;
+import com.example.maktabproject.exception.NotTheCorrectTimeToChangeStatusException;
+import com.example.maktabproject.exception.OrderNotFoundException;
+import com.example.maktabproject.model.Expert;
+import com.example.maktabproject.model.Offer;
+import com.example.maktabproject.model.Order;
+import com.example.maktabproject.model.enumeration.OrderState;
+import com.example.maktabproject.repository.OfferRepository;
+import com.example.maktabproject.repository.OrderRepository;
+import com.example.maktabproject.service.ExpertService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceTest {
+
+    @Mock
+    private OrderRepository orderRepository;
+    @Mock
+    private OfferRepository offerRepository;
+    @Mock
+    private ExpertService expertService;
+    @Mock
+    private CustomerServiceImpl customerService;
+
+    private OrderServiceImpl orderService;
+
+    @BeforeEach
+    void setup(){
+        orderService = new OrderServiceImpl(orderRepository,offerRepository,expertService,customerService);
+    }
+
+    @Test
+    void orderStateShouldChangeWhenExpertComes() throws OrderNotFoundException, InvalidPriceException, InvalidTimeException, NotTheCorrectTimeToChangeStatusException {
+
+        Order order = Order.builder()
+                .selectedOffer(Offer.builder()
+                        .expert(new Expert())
+                        .startingDate(LocalDateTime.now().plusSeconds(300))
+                        .build())
+                .build();
+
+        Mockito.when(orderRepository.findById(1L)).thenReturn(Optional.of(order));
+        Mockito.when(offerRepository.findByExpertAndOrder(any(Expert.class), any(Order.class))).thenReturn(Offer.builder()
+                        .startingDate(LocalDateTime.now().minusDays(1))
+                        .build());
+        Mockito.when(orderRepository.save(any(Order.class))).thenReturn(new Order());
+
+        Order order1 = orderService.statusToStarted(1L);
+        assertThat(order1).isNotNull();
+    }
+}
