3a89f3ba127e40d6a98d4e789282852a6fa3788c
Shahrad <shahradbagheri6@gmail.com>
2023-11-10 17:00:21 +0330

    created SecurityConfig

---
+++ src/main/java/com/example/maktabproject/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.maktabproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(a -> a.anyRequest().permitAll());
+        return httpSecurity.build();
+    }
+}
