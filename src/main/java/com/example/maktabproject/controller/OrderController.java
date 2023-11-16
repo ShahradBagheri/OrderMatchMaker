bf7909ece68532d91f8e82d097dd8b7e8fa25e40
Shahrad <shahradbagheri6@gmail.com>
2023-11-16 10:16:20 +0330

    select offer and all change order states are now authorized

--- src/main/java/com/example/maktabproject/controller/OrderController.java
+++ src/main/java/com/example/maktabproject/controller/OrderController.java
@@ -3,16 +3,20 @@ package com.example.maktabproject.controller;
 import com.example.maktabproject.dto.OrderMapper;
 import com.example.maktabproject.dto.OrderResponseDto;
 import com.example.maktabproject.exception.*;
+import com.example.maktabproject.model.Customer;
 import com.example.maktabproject.model.Order;
+import com.example.maktabproject.service.Impl.CustomerServiceImpl;
 import com.example.maktabproject.service.Impl.ExpertServiceImpl;
 import com.example.maktabproject.service.Impl.OrderServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/order")
@@ -22,8 +26,10 @@ public class OrderController {
     private final OrderServiceImpl orderService;
     private final OrderMapper orderMapper;
     private final ExpertServiceImpl expertService;
+    private final CustomerServiceImpl customerService;
 
     @GetMapping("/findForExpert")
+    @PreAuthorize("hasRole('EXPERT')")
     public ResponseEntity<List<OrderResponseDto>> findOrderForExpert() throws ExpertNotFoundException {
 
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -35,25 +41,53 @@ public class OrderController {
     }
 
     @PostMapping("/selectOffer")
-    public void selectOffer(@RequestParam Long offerId, @RequestParam Long orderId) throws OrderNotFoundException, InvalidPriceException, InvalidTimeException, ExpertHasNoOfferForOfferException, OfferNotFoundException {
+    @PreAuthorize("hasRole('CUSTOMER')")
+    public void selectOffer(@RequestParam Long offerId, @RequestParam Long orderId) {
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Customer customer = customerService.findByUsername(username);
+
+        if(!Objects.equals(orderService.findById(orderId).getCustomer().getId(), customer.getId()))
+            throw new NotOrderOwnerException("you dont own this order!");
 
         orderService.choseOffer(offerId, orderId);
     }
 
     @PostMapping("/changeState/started")
-    public ResponseEntity<OrderResponseDto> changeStateStarted(@RequestParam Long orderId) throws OrderNotFoundException, InvalidPriceException, InvalidTimeException, NotTheCorrectTimeToChangeStatusException {
+    @PreAuthorize("hasRole('CUSTOMER')")
+    public ResponseEntity<OrderResponseDto> changeStateStarted(@RequestParam Long orderId) {
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Customer customer = customerService.findByUsername(username);
+
+        if(!Objects.equals(orderService.findById(orderId).getCustomer().getId(), customer.getId()))
+            throw new NotOrderOwnerException("you dont own this order!");
 
         return new ResponseEntity<>(orderMapper.orderToDto(orderService.statusToStarted(orderId)), HttpStatus.OK);
     }
 
     @PostMapping("/changeState/finished")
+    @PreAuthorize("hasRole('CUSTOMER')")
     public ResponseEntity<OrderResponseDto> changeStateFinished(@RequestParam Long orderId) throws OrderNotFoundException, InvalidPriceException, InvalidTimeException, NotTheCorrectTimeToChangeStatusException {
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Customer customer = customerService.findByUsername(username);
+
+        if(!Objects.equals(orderService.findById(orderId).getCustomer().getId(), customer.getId()))
+            throw new NotOrderOwnerException("you dont own this order!");
 
         return new ResponseEntity<>(orderMapper.orderToDto(orderService.statusToFinished(orderId)), HttpStatus.OK);
     }
 
     @PostMapping("/changeState/paid")
+    @PreAuthorize("hasRole('CUSTOMER')")
     public ResponseEntity<OrderResponseDto> changeStatePaid(@RequestParam Long orderId) throws OrderNotFoundException, InvalidPriceException, InvalidTimeException, ExpertNotFoundException, InsufficientFundException, CustomerNotFoundException {
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Customer customer = customerService.findByUsername(username);
+
+        if(!Objects.equals(orderService.findById(orderId).getCustomer().getId(), customer.getId()))
+            throw new NotOrderOwnerException("you dont own this order!");
 
         return new ResponseEntity<>(orderMapper.orderToDto(orderService.statusToPaid(orderId)), HttpStatus.OK);
     }
