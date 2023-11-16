b13a45f62cde2eff6ffbecdf6700d4c241d81c48
Shahrad <shahradbagheri6@gmail.com>
2023-11-16 20:07:34 +0330

    added mapper to convert orderFilter dtos

--- src/main/java/com/example/maktabproject/dto/OrderFilterCriteriaDto.java
+++ src/main/java/com/example/maktabproject/dto/OrderFilterCriteriaDto.java
@@ -5,9 +5,11 @@ import com.example.maktabproject.model.Expert;
 import com.example.maktabproject.model.MainService;
 import com.example.maktabproject.model.SubService;
 import com.example.maktabproject.model.enumeration.OrderState;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record OrderFilterCriteriaDto(Customer customer,
                                      Expert expert,
                                      LocalDateTime doneAfter,
--- src/main/java/com/example/maktabproject/dto/OrderMapper.java
+++ src/main/java/com/example/maktabproject/dto/OrderMapper.java
@@ -2,8 +2,10 @@ package com.example.maktabproject.dto;
 
 import com.example.maktabproject.exception.CustomerNotFoundException;
 import com.example.maktabproject.exception.SubServiceNotFoundException;
-import com.example.maktabproject.model.Order;
+import com.example.maktabproject.model.*;
 import com.example.maktabproject.service.Impl.CustomerServiceImpl;
+import com.example.maktabproject.service.Impl.ExpertServiceImpl;
+import com.example.maktabproject.service.Impl.MainServiceServiceImpl;
 import com.example.maktabproject.service.Impl.SubServiceServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -14,6 +16,8 @@ public class OrderMapper {
 
     private final SubServiceServiceImpl subServiceService;
     private final CustomerServiceImpl customerService;
+    private final ExpertServiceImpl expertService;
+    private final MainServiceServiceImpl mainServiceService;
 
     public Order dtoToOrder(OrderRequestDto orderRequestDto) throws SubServiceNotFoundException, CustomerNotFoundException {
         return Order.builder()
@@ -34,6 +38,32 @@ public class OrderMapper {
                 .startingDate(order.getStartingDate())
                 .details(order.getDetails())
                 .address(order.getAddress())
+                .build();
+    }
+
+    public OrderFilterCriteriaDto dtoToCriteria(OrderFilterRequestDto orderFilterRequestDto) {
+
+        Expert expert = null;
+        Customer customer = null;
+        SubService subService = null;
+        MainService mainService = null;
+
+        if (orderFilterRequestDto.customerId() != null)
+            customer = customerService.findById(orderFilterRequestDto.customerId());
+        if (orderFilterRequestDto.expertId() != null)
+            expert = expertService.findById(orderFilterRequestDto.expertId());
+        if (orderFilterRequestDto.subServiceId() != null)
+            subService = subServiceService.findById(orderFilterRequestDto.subServiceId());
+        if (orderFilterRequestDto.mainServiceId() != null)
+            mainService = mainServiceService.findById(orderFilterRequestDto.mainServiceId());
+
+        return OrderFilterCriteriaDto.builder()
+                .customer(customer)
+                .expert(expert)
+                .doneAfter(orderFilterRequestDto.doneAfter())
+                .doneBefore(orderFilterRequestDto.doneBefore())
+                .subService(subService)
+                .mainService(mainService)
                 .build();
     }
 }
