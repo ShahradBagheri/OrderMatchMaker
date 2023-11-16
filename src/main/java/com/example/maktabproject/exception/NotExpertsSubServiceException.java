6fa576ffc2a8969dea3a0ed08873782b6a5e4107
Shahrad <shahradbagheri6@gmail.com>
2023-11-17 02:51:03 +0330

    created NotExpertsSubServiceException

---
+++ src/main/java/com/example/maktabproject/exception/NotExpertsSubServiceException.java
@@ -0,0 +1,11 @@
+package com.example.maktabproject.exception;
+
+public class NotExpertsSubServiceException extends RuntimeException{
+
+    public NotExpertsSubServiceException() {
+    }
+
+    public NotExpertsSubServiceException(String message) {
+        super(message);
+    }
+}
