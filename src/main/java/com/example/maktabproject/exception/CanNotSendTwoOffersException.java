3b4b0dca0e65bdf6c0701cf4bc952c65cf94833f
Shahrad <shahradbagheri6@gmail.com>
2023-11-17 02:55:40 +0330

    created CanNotSendTwoOffersException

---
+++ src/main/java/com/example/maktabproject/exception/CanNotSendTwoOffersException.java
@@ -0,0 +1,11 @@
+package com.example.maktabproject.exception;
+
+public class CanNotSendTwoOffersException extends RuntimeException{
+
+    public CanNotSendTwoOffersException() {
+    }
+
+    public CanNotSendTwoOffersException(String message) {
+        super(message);
+    }
+}
