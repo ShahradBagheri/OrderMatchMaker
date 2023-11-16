d5c9b477f65ecc298bdc56366d66d3029393ff1a
Shahrad <shahradbagheri6@gmail.com>
2023-11-17 02:59:05 +0330

    added ConstraintViolationException and DataAccessException to GlobalExceptionHandler

--- src/main/java/com/example/maktabproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/maktabproject/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.maktabproject.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -132,6 +134,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<String> nullPointerException(NullPointerException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> nullPointerException(ConstraintViolationException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> nullPointerException(DataAccessException e) {
         log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
