1393b29a8777324a9a0ab53be8d4c051af0a1c74
Shahrad <shahradbagheri6@gmail.com>
2023-11-16 17:51:42 +0330

    updated field and methods

---
+++ src/main/java/com/example/maktabproject/model/VerificationToken.java
@@ -0,0 +1,30 @@
+package com.example.maktabproject.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class VerificationToken {
+    private static final int EXPIRATION = 60 * 24;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private LocalDateTime expiryDate;
+
+    private LocalDateTime calculateExpiryDate(int expiryTimeInMinutes) {
+        return LocalDateTime.now().plusMinutes(expiryTimeInMinutes);
+    }
+}
