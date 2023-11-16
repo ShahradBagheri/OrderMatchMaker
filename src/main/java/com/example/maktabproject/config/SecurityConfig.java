5336cb1e5dfaa27691173527032275e9f0b5d7bd
Shahrad <shahradbagheri6@gmail.com>
2023-11-16 08:22:34 +0330

    configured security

--- src/main/java/com/example/maktabproject/config/SecurityConfig.java
+++ src/main/java/com/example/maktabproject/config/SecurityConfig.java
@@ -1,18 +1,44 @@
 package com.example.maktabproject.config;
 
+import com.example.maktabproject.exception.UserNotFoundException;
+import com.example.maktabproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(a -> a.anyRequest().permitAll());
+        httpSecurity
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a.anyRequest().permitAll()).httpBasic(withDefaults());
         return httpSecurity.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(username -> userRepository
+                .findByUsername(username).orElseThrow(() -> new UserNotFoundException(username + " not Found")))
+                .passwordEncoder(passwordEncoder);
     }
 }
